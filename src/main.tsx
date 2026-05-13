84a6eb2595627858cf969ff498e3be3975d74c72
muzamilmeerdev <127821271+muzamilmeerdev@users.noreply.github.com>
2026-05-13 17:17:09 +0530

    Import global and theme styles in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 
+import "./styles/globals.css";
+import "./styles/tailwind.css";
+import "./styles/theme.css";
+import "../default_shadcn_theme.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
